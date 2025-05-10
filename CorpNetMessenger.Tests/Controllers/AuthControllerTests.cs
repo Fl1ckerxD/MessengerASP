f902efbaca809ea0255b2c72a41a6f8df6bc0ff4
Fl1ckerxD <mihaylav.slava@yandex.ru>
2025-05-10 16:30:37 +0300

    added tests

---
+++ CorpNetMessenger.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using CorpNetMessenger.Domain.Entities;
+using CorpNetMessenger.Domain.Interfaces.Services;
+using CorpNetMessenger.Web.Controllers;
+using CorpNetMessenger.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CorpNetMessenger.Tests.Controllers
+{
+    public class AuthControllerTests
+    {
+        private Mock<SignInManager<User>> _signInManagerMock;
+
+        public AuthControllerTests()
+        {
+            var userStore = new Mock<IUserStore<User>>();
+            var userManager = new Mock<UserManager<User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            _signInManagerMock = new Mock<SignInManager<User>>(userManager.Object, Mock.Of<Microsoft.AspNetCore.Http.IHttpContextAccessor>(), Mock.Of<Microsoft.AspNetCore.Identity.IUserClaimsPrincipalFactory<User>>(), null, null, null, null);
+        }
+
+        [Fact]
+        public async Task Login_Post_SuccessfulLogin_RedirectsToHome()
+        {
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.Login(It.IsAny<LoginViewModel>()))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+
+            var controller = new AuthController(Mock.Of<IMapper>(), accountServiceMock.Object, _signInManagerMock.Object);
+            var model = new LoginViewModel();
+
+            var result = await controller.Login(model);
+
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+            Assert.Equal("Home", redirectResult.ControllerName);
+        }
+
+        [Fact]
+        public async Task Login_Post_FailedLogin_AddsErrorAndView()
+        {
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.Login(It.IsAny<LoginViewModel>()))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Failed);
+
+            var controller = new AuthController(Mock.Of<IMapper>(), accountServiceMock.Object, _signInManagerMock.Object);
+            var model = new LoginViewModel();
+            
+            var result = await controller.Login(model);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same(model, viewResult.Model);
+            Assert.True(controller.ModelState.ErrorCount > 0);
+        }
+
+        [Fact]
+        public async Task Register_Post_SuccessfulRegistration_RedirectsToLogin()
+        {
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.Register(It.IsAny<RegisterViewModel>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var controller = new AuthController(Mock.Of<IMapper>(), accountServiceMock.Object, _signInManagerMock.Object);
+            var model = new RegisterViewModel();
+
+            var result = await controller.Register(model);
+
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Login", redirectResult.ActionName);
+        }
+
+        [Fact]
+        public async Task Register_Post_HasErrors_AddsModelErrorsAndView()
+        {
+            var errors = new[] { new IdentityError { Description = "Error1" }, new IdentityError { Description = "Error2" } };
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.Register(It.IsAny<RegisterViewModel>()))
+                .ReturnsAsync(IdentityResult.Failed(errors));
+
+            var controller = new AuthController(Mock.Of<IMapper>(), accountServiceMock.Object, _signInManagerMock.Object);
+            var model = new RegisterViewModel();
+
+            var result = await controller.Register(model);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same(model, viewResult.Model);
+            Assert.Equal(2, controller.ModelState.ErrorCount);
+        }
+    }
+}
---
+++ CorpNetMessenger.Tests/Services/AccountServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CorpNetMessenger.Domain.Entities;
+using CorpNetMessenger.Infrastructure.Services;
+using CorpNetMessenger.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace CorpNetMessenger.Tests.Services
+{
+    public class AccountServiceTests
+    {
+        private Mock<UserManager<User>> _userManagerMock;
+        private Mock<SignInManager<User>> _signInManagerMock;
+        public AccountServiceTests()
+        {
+            var userStore = new Mock<IUserStore<User>>();
+            _userManagerMock = new Mock<UserManager<User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            _signInManagerMock = new Mock<SignInManager<User>>(_userManagerMock.Object, Mock.Of<Microsoft.AspNetCore.Http.IHttpContextAccessor>(), Mock.Of<Microsoft.AspNetCore.Identity.IUserClaimsPrincipalFactory<User>>(), null, null, null, null);
+        }
+
+        [Fact]
+        public async Task Login_CallsPasswordSignInAsync_WithCorrectParameters()
+        {
+            var model = new LoginViewModel { UserName = "test", Password = "password", RememberMe = true };
+
+            var accountService = new AccountService(Mock.Of<IMapper>(), _userManagerMock.Object, _signInManagerMock.Object);
+
+            await accountService.Login(model);
+
+            _signInManagerMock.Verify(x => x.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false), Times.Once);
+        }
+    }
+}
